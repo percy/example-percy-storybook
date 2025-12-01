08970a9d90bc0ba897b399b8c7895f57ef8e3bc8
Kenton Self <kenton.s@browserstack.com>
2025-12-01 13:33:49 -0600

    change to ital

--- js/TodoFooter.js
+++ js/TodoFooter.js
@@ -34,7 +34,7 @@ export default function TodoFooter({
 
       {completedCount > 0 ? (
         <button
-          style={{ fontWeight: 'bold' }}
+          style={{ fontWeight: 'italic' }}
           className="clear-completed"
           onClick={onClearCompleted}>
           Clear completed
